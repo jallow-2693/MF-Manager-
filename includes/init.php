2cf39e9377b098b3f696e0d47239e0a5a0034113
jallow-2693 <diallomaoudia13466@gmail.com>
2025-03-31 19:05:35 +0000

    Create init.php

---
+++ includes/init.php
@@ -0,0 +1,7 @@
+<?php
+// Inclusion automatique du fichier config.php
+include_once('config.php');
+
+// Tu peux ajouter d'autres fichiers ici si nécessaire
+// Par exemple, inclure des fonctions ou des classes que tu utilises souvent
+?>
