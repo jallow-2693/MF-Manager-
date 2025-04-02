3e64c157c3b07893d310ddac41b69751b3de4a7d
jallow-2693 <diallomaoudia13466@gmail.com>
2025-04-02 12:51:33 +0000

    Delete includes directory

--- includes/init.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Inclusion automatique du fichier config.php
-include_once('config.php');
-
-// Tu peux ajouter d'autres fichiers ici si nécessaire
-// Par exemple, inclure des fonctions ou des classes que tu utilises souvent
-?>
